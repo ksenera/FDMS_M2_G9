2d6fc0fd2c99144af955de760fce519dc2e4d2e1
Kushika Senera <ksenera7130@conestogac.on.ca>
2024-11-21 01:07:49 -0500

    readded the projects from FDMS old directory due to numerous git cache issues

---
+++ AircraftTransmissionSystem/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace AircraftTransmissionSystem
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
---
+++ SharedLibrary/ParsedData.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SharedLibrary
+{
+    public class ParsedData
+    {
+        public string AircraftID { get; set; }
+        public DateTime Timestamp { get; set; }
+        public double Altitude { get; set; }
+        public double Pitch { get; set; }
+        public double Bank { get; set; }
+
+    }
+}
