ed1064e8e6d5782252fa05c9f51b80f033f6b1c9
Andrew3369 <andrewjanosbabos@gmail.com>
2024-11-21 14:24:49 -0500

    Created PacketBuilder.cs
    Outline for packet builder module
    commented out the class for the Packet, struct might be better to use...

---
+++ AircraftTransmissionSystem/PacketBuilder.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AircraftTransmissionSystem
+{
+    internal class PacketBuilder
+    {
+        private struct Packet
+        {
+            private string header;
+            private string packetBody;
+            private Int32 checksum;
+        }
+
+        private string Header { get; set; }
+        private string PacketBody { get; set; }
+        private int Checksum { get; set; }
+
+        public void AddHeader(string header)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void AddBody(string data)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void AddChecksum()
+        {
+            throw new NotImplementedException();
+        }
+
+        public string GetPacket()
+        {
+            throw new NotImplementedException();
+        }
+
+        private bool VerifyChecksum(Packet packet)
+        {
+            throw new NotImplementedException();
+        }
+
+        //private class Packet
+        //{
+
+        //    public Packet(string header, string packetBody, string checksum)
+        //    {
+        //        this.header = header;
+        //        this.packetBody = packetBody;
+        //        this.checksum = checksum;
+        //    }
+
+        //    public bool VerifyCheckSum()
+        //    {
+        //        throw new NotImplementedException();
+        //    }
+        //}
+    }
+}
