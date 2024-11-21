fa820bec79304a9363553d907c21ba3b77025908
Kushika Senera <ksenera7130@conestogac.on.ca>
2024-11-21 01:16:40 -0500

    copied and pasted TelemetryParser from other solution folder due to numerous gitignore issues on the original repository

---
+++ GroundStationTerminal/TelemetryParser.cs
@@ -0,0 +1,40 @@
+﻿using SharedLibrary;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GroundStationTerminal
+{
+    public class TelemetryParser
+    {
+        private string dataFormat;
+
+        public TelemetryParser(string format)
+        {
+            this.dataFormat = format;
+        }
+
+        public ParsedData ParseData(string data)
+        {
+
+            // if the data is not in the correct format, throw ArgumentException
+
+            // parse based on specific format array of strings and split
+
+            return new ParsedData();
+            // should follow the format as per SharedLibrary.ParsedData
+        }
+
+        public bool ValidateChecksum((DateTime Timestamp, double AccelX, double AccelY, double AccelZ,
+            double Weight, double Altitude, double Pitch, double Bank) body, int providedChecksum)
+        {
+            double checksumValue = (body.Altitude + body.Pitch + body.Bank) / 3;
+
+            int calculatedChecksum = (int)Math.Round(checksumValue);
+
+            return providedChecksum == calculatedChecksum;
+        }
+    }
+}
