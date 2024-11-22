5be3da16c55ff746e18460495f0f43375a6c7566
Kushika Senera <ksenera7130@conestogac.on.ca>
2024-11-22 17:16:09 -0500

    added main program to ground station to set up instances of parser and collector to start the tcp listener and loop to receive and listen to data (not fully implemented)

---
+++ GroundStationTerminal/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GroundStationTerminal
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            TelemetryParser telemetryParser = new TelemetryParser("default_format");
+            TelemetryCollector telemetryCollector = new TelemetryCollector("127.0.0.1", 8080, telemetryParser);
+
+            // start the listener here 
+            Console.WriteLine("Listening for incoming aircraft transmissions...");
+            telemetryCollector.Connect();
+
+            // loop to receive the telemetry data and call the correct classes...
+            while (true)
+            {
+                telemetryCollector.ReceiveData(); // continually listen for live updates 
+            }
+        }
+    }
+}
